8e1ec82537f40bb50aa6275296ed150a03e3daf4
HazardJ <jamesghazard@gmail.com>
2017-10-08 13:09:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   
   Kea, one of the Cycladic islands in Greece, lived for eons on farming, much of it on terraced hillsides, and a bit of trade.  In classical times, Kea had four cities, including Karthaia, Ioulis, Korissia and Pisses.  In modern times, Ioulis became the capital of the island and Korissia thrived as the port.<br>
   Paradoxically, the island's proximity to the Greek mainland kept it isolated.  The ferry to Kea left from Lavrion, the town where the silver mines in ancient times enabled the Athenians to build a fleet that overcame Darius's navy.  Even into the 1990s there were few roads and donkeys were a major form of transport.  Only recently has Lavrion been revived, and the ferries to Kea made much more reliable.  The island is in transition from its older subsistence agriculture to something more complex.
- <br>KIHP-IT is intended to help preserve some of the unique human knowledge and connection to the past, while enabling others to study, learn and benefit from that knowledge.<br>  
+ <br>KIHP-IT is intended to help preserve some of the unique human knowledge, resources and connection to the past, while enabling others to study, learn and interact with that unique whole.<br>  
   
 <br><br>
 Resources:
