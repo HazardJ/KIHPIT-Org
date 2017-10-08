8ea58fcf8c1166f64e27ef9d9daa4a5fee760179
bnjhzrd <bnjhzrd@gmail.com>
2017-10-08 16:29:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
-<center><h1>KIHP-IT<h1><h4>(suggested pronunciation "keep it")</h4>
+<html><head>
+ <link rel = stylesheet type=text/css href=style.css>
+</head>
+<body><center><h1>KIHP-IT<h1><h4>(suggested pronunciation "keep it")</h4>
   <h3>The Kean Institute for Human Progress - in Transition</h3></center>
 
-  <link rel = stylesheet type=text/css href=style.css>
+ 
   
   Kea, one of the Cycladic islands in Greece, lived for eons on farming, much of it on terraced hillsides, and a bit of trade.  In classical times, Kea had four cities, including Karthaia, Ioulis, Korissia and Pisses.  In modern times, Ioulis became the capital of the island and Korissia thrived as the port.<br>
   Paradoxically, the island's proximity to the Greek mainland kept it isolated.  The ferry to Kea left from Lavrion, the town where the silver mines in ancient times enabled the Athenians to build a fleet that overcame Darius's navy.  Even into the 1990s there were few roads; donkeys were a principal form of transport.  Only recently has Lavrion been revived and the ferries to Kea made much more reliable.  The island is in transition from its older subsistence agriculture to something more complex.
@@ -10,3 +13,5 @@
 <br><br>
 Resources:
 <ol><li>Description:<br><a href="http://www.kea.gr/index.php/en/">kea.gr/index.php/en/ (official site)</a><br><a href="https://en.wikipedia.org/wiki/Kea_(island)">Kea (Wikipedia/EN)<br><a href="http://www.greektravel.com/greekislands/kea/">www.greektravel.com/greekislands/kea/</a></ol> 
+  </body>
+  </html>
